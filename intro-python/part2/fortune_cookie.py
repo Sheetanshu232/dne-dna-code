b799530adb6d6e9c6ac85fc444a01b5134f41b46
SHEETANSHU <sheetanshu.shekhar@gmail.com>
2020-12-05 00:47:00 +0530

    Completed the fortune cookie assignment

--- intro-python/part2/fortune_cookie.py
+++ intro-python/part2/fortune_cookie.py
@@ -34,7 +34,9 @@ def create_fortune_cookie_message(how_many_lucky_numbers: int) -> str:
     # TODO: Create a fortune cookie message by calling generate_fortune() and
     # generate_lucky_numbers() and then composing and returning the fortune
     # cookie's message.
-
+    fortune = generate_fortune()
+    lucky_number = generate_lucky_numbers(how_many_lucky_numbers)
+    return"{}\nLucky Numbers: {}".format(fortune,lucky_number)
     raise NotImplementedError()
 
 
